277e9460ca5d15cefd0e7f90075922da1e3a39be
Adrian Aguilar <aj@Mac.AirPort>
2026-06-12 00:31:50 +0900

    add firebase config keys

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,11 @@
 // This public configuration identifies your Firebase project; security comes from
 // Firebase Authentication and the Firestore rules in firestore.rules.
 export const firebaseConfig = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: "AIzaSyCwyDXkq21HG8dbEQsDFX9rg20L3kpcnpo",
+    authDomain: "ninjalearn-lab.firebaseapp.com",
+    projectId: "ninjalearn-lab",
+    storageBucket: "ninjalearn-lab.firebasestorage.app",
+    messagingSenderId: "27949382313",
+    appId: "1:27949382313:web:e8da0bfa353e8ab3704af8",
+    measurementId: "G-3WLE34WWQR"
 };
